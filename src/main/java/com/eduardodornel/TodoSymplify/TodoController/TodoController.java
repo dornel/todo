3c5c382f9ccf4c9ff474c05e5b135f785626e17d
Eduardo Dornel <dornel.eduardo@gmail.com>
2024-08-21 11:40:39 -0300

    ToDo Tool - Simplyfy Test

--- src/main/java/com/eduardodornel/TodoSymplify/TodoController/TodoController.java
+++ src/main/java/com/eduardodornel/TodoSymplify/TodoController/TodoController.java
@@ -8,21 +8,20 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
+@RequestMapping("/todo")
 public class TodoController {
 
     @Autowired
     TodoRepository todoRepository;
 
-    @PostMapping("/todo")
-    public ResponseEntity<TodoModel> saveTodo(@RequestBody @Valid TodoRecordDto todoRecordDto){
+    @PostMapping
+       public ResponseEntity<TodoModel> saveTodo(@RequestBody @Valid TodoRecordDto todoRecordDto){
 
         var todoModel = new TodoModel();
         BeanUtils.copyProperties(todoRecordDto,todoModel);
@@ -30,7 +29,7 @@ public class TodoController {
 
     }
 
-    @GetMapping("/todo")
+    @GetMapping
     public ResponseEntity<List<TodoModel>> getAllTodo(){
         return ResponseEntity.status(HttpStatus.OK).body(todoRepository.findAll());
     }
